f9c872daf474b84f64dafc3c709a7378c833795c
gabriel <19943@studenti.marconiverona.edu.it>
2023-10-26 19:44:12 +0200

    modificato es 5

--- TriangoloPascal.java
+++ TriangoloPascal.java
@@ -10,32 +10,60 @@ public class TriangoloPascal {
         // dichiarazione
         Scanner input = new Scanner(System.in);
         int numRighe;
-        String rigaP = "";
-        String riga = "";
-        int somma;
+        int[] rigaP = new int[34];
+        Arrays.fill(rigaP, 0);
+        int[] riga = new int[34];
+        Arrays.fill(riga, 0);
+        boolean empty;
+        String rigaF = "";
 
-        // input
+        // input righe
         do {
-            System.out.print("Inserire numero righe (1 - 100): ");
+            System.out.print("Inserire numero righe (1 - 34): ");
             numRighe = Integer.parseInt(input.nextLine());
-        } while (numRighe <= 0 || numRighe > 100);
+        } while (numRighe <= 0 || numRighe > 34); // ci sono problemi se > 34
 
+        // ciclo riga per riga
         for (int i = 0; i < numRighe; i++) {
-            rigaP = riga;
-            riga = "";
-            somma = 0;
-            if (rigaP == "")
-                riga = "1";
-            else {
-                riga += "1";
-                for (int j = 0; j < i - 1; j++) {
-                    somma = Character.getNumericValue(rigaP.charAt(j)) +
-                        Character.getNumericValue(rigaP.charAt(j + 1));
-                    riga += somma + "";
-                }
-                riga += "1";
+
+            // copia riga in rigaP
+            for (int e = 0; e < riga.length; e++) {
+                rigaP[e] = riga[e];
             }
-            System.out.println(riga);
+
+            // azzeramento riga e rigaF
+            Arrays.fill(riga, 0);
+            rigaF = "";
+
+            // controllo se rigaP è vuoto
+            empty = true;
+            for (int e = 0; e < rigaP.length; e++) {
+                if (rigaP[e] != 0) {
+                    empty = false;
+                }
+            }
+
+            // codice se rigaP è vuoto (eseguito solo una volta all'inizio)
+            if (empty) {
+                riga[0] = 1;
+            }
+
+            // altrimenti...
+            else {
+                riga[0] = 1;
+                for (int j = 1; j < i; j++) {
+                    riga[j] = rigaP[j - 1] + rigaP[j];
+                }
+                riga[i] = 1;
+            }
+
+            // riempimento rigaF
+            for (int j = 0; j < riga.length && riga[j] != 0; j++) {
+                rigaF += riga[j] + " ";
+            }
+
+            // output
+            System.out.println(rigaF);
         }
     }
 }
